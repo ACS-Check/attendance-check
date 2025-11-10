c7df5a4d661cad90f260f7c6f745745e59db3596
Hyeonbeen Park <hyunbini2942@gmail.com>
2025-11-10 09:37:35 +0900

    feat: DB 연결 설정 및 유틸 클래스 추가

---
+++ src/main/java/attendance/connection/ConnectionConst.java
@@ -0,0 +1,7 @@
+package attendance.connection;
+
+public abstract class ConnectionConst {
+    public static final String URL = "jdbc:mysql://localhost:3306/attendance";
+    public static final String USERNAME = "root";
+    public static final String PASSWORD = "gusqls96%";
+}
